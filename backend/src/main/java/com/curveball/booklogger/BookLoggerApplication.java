f6c3bc77e46cba512519544ac2df83126afd4032
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2021-09-25 11:57:41 +0100

    removed unused import

--- backend/src/main/java/com/curveball/booklogger/BookLoggerApplication.java
+++ backend/src/main/java/com/curveball/booklogger/BookLoggerApplication.java
@@ -1,12 +1,10 @@
 package com.curveball.booklogger;
 
-import com.curveball.booklogger.book.BookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookLoggerApplication {
-    BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookLoggerApplication.class, args);
